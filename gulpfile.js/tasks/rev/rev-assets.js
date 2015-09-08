f3337254e3f6ee36811cb780055b86a9401a56b9
Dan Tello <dan.tello@gmail.com>
2015-09-08 12:34:06 -0400

    [in progress] fix iconFont task

--- gulpfile.js/tasks/rev/rev-assets.js
+++ gulpfile.js/tasks/rev/rev-assets.js
@@ -1,16 +1,15 @@
 var config = require('../../config');
-var iconFontConfig = require('../../config/iconFont');
 var gulp   = require('gulp');
 var rev    = require('gulp-rev');
 
 // 1) Add md5 hashes to assets referenced by CSS and JS files
 gulp.task('rev-assets', function() {
   // Ignore what we dont want to hash in this step
-  var notThese = '!' + config.publicDirectory + '/**/*+(css|js|json|html)'
+  var notThese = '!' + config.dest.root + '/**/*+(css|js|json|html)'
 
-  return gulp.src([config.publicDirectory + '/**/*', notThese])
+  return gulp.src([config.dest.root + '/**/*', notThese])
     .pipe(rev())
-    .pipe(gulp.dest(config.publicDirectory))
-    .pipe(rev.manifest('public/rev-manifest.json', {merge: true}))
+    .pipe(gulp.dest(config.dest.root))
+    .pipe(rev.manifest(config.dest.root.substr(2) + '/rev-manifest.json', {merge: true}))
     .pipe(gulp.dest(''));
 });
